f3de25a346a8b1492593b6dbfa3fd355809dbaf0
quantxz <indiedev.core@gmail.com>
2023-06-22 10:29:57 -0300

    update

--- src/configs/knexConfig.ts
+++ src/configs/knexConfig.ts
@@ -1,5 +1,4 @@
 export const knexPg = require('knex')({
     client: 'pg',
-    connection: 'postgres://quantxz:0jIQpekrcX4P@ep-black-meadow-971825-pooler.us-east-2.aws.neon.tech/neondb',
-    searchPath: ['knex', 'public'],
+    connection: 'postgres://quantxz:0jIQpekrcX4P@ep-black-meadow-971825-pooler.us-east-2.aws.neon.tech/neondb'
 });
