54fdd556682f96475f411b72d5c647c07ea141b7
quantxz <indiedev.core@gmail.com>
2023-06-22 10:38:34 -0300

    update

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const routes: Router = Router();
 
 routes.get("/", async (req: Request, res: Response) => {
-    const episodes = await PrismaClient.episodes.findMany()
     
-    return res.json(episodes)
+    return res.json("episodes")
 });
 
 export default routes;
