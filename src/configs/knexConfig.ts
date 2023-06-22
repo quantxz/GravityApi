82ea6061508ad139e374695fc374595c7862db02
quantxz <indiedev.core@gmail.com>
2023-06-22 10:33:49 -0300

    update

--- src/configs/knexConfig.ts
+++
@@ -1,4 +0,0 @@
-export const knexPg = require('knex')({
-    client: 'pg',
-    connection: 'postgres://quantxz:0jIQpekrcX4P@ep-black-meadow-971825-pooler.us-east-2.aws.neon.tech/neondb'
-});
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import { Request, Response, Router } from "express";
-import { knexPg }   from "./configs/knexConfig";
 const routes: Router = Router();
 
 routes.get("/", (req: Request, res: Response) => {
-    return res.json(knexPg('episodes').select("*"))
+    return res.json("deu certo")
 });
 
 export default routes;
