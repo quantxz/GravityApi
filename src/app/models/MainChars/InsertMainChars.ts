d9afd14a8ad53875d2d4c255b619a2e715927cd0
quantxz <indiedev.core@gmail.com>
2023-06-22 17:52:32 -0300

    update

--- src/app/models/MainChars/InsertMainChars.ts
+++ src/app/models/MainChars/InsertMainChars.ts
@@ -3,7 +3,7 @@ import { mainCharsInterface } from "../../interfaces";
 
 export const createMainCharacters = async (data: mainCharsInterface) => {
     try {
-        const query = "INSERT INTO mainChars (name, first_episode, last_episode, voice, age, born, ocuppation, inspiration, likes, dislikes, description, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)"
+        const query = "INSERT INTO mainchars (name, first_episode, last_episode, voice, age, born, ocuppation, inspiration, likes, dislikes, description, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)"
         const values = [data.name, data.fisrt_episode, data.last_episode, data.voice, data.age, data.born, data.ocuppation, data.inspiration, data.likes, data.dislikes, data.description, data.image]
 
         return await pool.query(query, values)
