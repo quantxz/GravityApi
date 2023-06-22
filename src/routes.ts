c3ff8cd175983bee63d4c12c706d393d23ece446
quantxz <indiedev.core@gmail.com>
2023-06-22 12:36:49 -0300

    update

--- src/routes.ts
+++ src/routes.ts
@@ -21,6 +21,24 @@ routes.get("/episodes", async (req: Request, res: Response) => {
   }
 });
 
+routes.post("/episodes", async (req: Request, res: Response) => {
+  try {
+    const { title, ptBr_title, first_exibition, season, directed_by, image, written_by } = req.body;
+
+    const query = 'INSERT INTO episodes (title, ptBr_title, first_exibition, season, directed_by, image, written_by) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+    const values = [title, ptBr_title, first_exibition, season, directed_by, image, written_by];
+
+    const data = await pool.query(query, values);
+
+    res.json(data);
+
+  } catch (err) {
+    console.error('Erro ao executar consulta:', err);
+    res.status(500).send('Erro ao executar consulta.');
+  }
+});
+
+
 routes.get("/books", async (req: Request, res: Response) => {
   try {
     const query = 'SELECT * FROM books';
