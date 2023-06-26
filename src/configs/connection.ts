2fe30e6081b828b01c9243da83ec014f684fdab1
quantxz <indiedev.core@gmail.com>
2023-06-26 20:19:25 -0300

    finish update

--- src/configs/connection.ts
+++ src/configs/connection.ts
@@ -6,8 +6,8 @@ const connection = mysql.createConnection({
   password: 'pscale_pw_pC0YCu6kFYYZNaMrXbCx1ohst5XYeOfYupKyMGs8Zg3',
   database: 'gfapi',
   ssl: {
-    rejectUnauthorized: true
-  }
+    rejectUnauthorized: false
+  },
 });
 
 export { connection };
