c916d534f415fbd044a61ebac8a1bc92fa27361b
quantxz <indiedev.core@gmail.com>
2023-06-26 20:56:58 -0300

    finish update

--- src/configs/connection.ts
+++ src/configs/connection.ts
@@ -4,7 +4,10 @@ const connection = mysql.createConnection({
   database: 'gfapi',
   user: 'ay547om749y2oqs4e3dw',
   host: 'aws.connect.psdb.cloud',
-  password: 'pscale_pw_5IEq8JAZ1CUaTw41BA5bQhWivFv4tZq7aINVoxkI4e'
+  password: 'pscale_pw_5IEq8JAZ1CUaTw41BA5bQhWivFv4tZq7aINVoxkI4e',
+  ssl: {
+    rejectUnauthorized: false
+  },
 });
 
 export { connection };
