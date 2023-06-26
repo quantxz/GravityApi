f0ed529b1b71ca627f55faf2c83e629536ed56d4
quantxz <indiedev.core@gmail.com>
2023-06-26 20:16:05 -0300

    finish update

--- src/configs/connection.ts
+++ src/configs/connection.ts
@@ -2,8 +2,8 @@ import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
   host: 'aws.connect.psdb.cloud',
-  user: 'z5v6j7t80dxt7rmi0pm0',
-  password: 'pscale_pw_bcHrjNmlZDyxWsh8CQXnHVrW0VdE17hHecHUXC0xTo6',
+  user: '4yn9c413ubj91bczunqg',
+  password: 'pscale_pw_pC0YCu6kFYYZNaMrXbCx1ohst5XYeOfYupKyMGs8Zg3',
   database: 'gfapi',
   ssl: {
     rejectUnauthorized: true
