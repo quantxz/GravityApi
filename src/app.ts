07a7ca2d2acbf37e5340ff43796d4fe307a3c909
quantxz <indiedev.core@gmail.com>
2023-06-29 12:52:16 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -6,31 +6,40 @@ import BooksRoutes from './routes/books.routes';
 import WeirdmageddonRoutes from './routes/Weirdmageddon.routes';
 import CreaturesRoutes from './routes/creatures.routes';
 import routes from './routes/main.routes';
-import cors from 'cors'
+import cors from 'cors';
 
 class App {
-    public server: express.Application;
+  public server: express.Application;
 
-    constructor() {
-        this.server = express();
-        this.middleware();
-        this.routes();
-    }
+  constructor() {
+    this.server = express();
+    this.middleware();
+    this.routes();
+  }
 
-    private middleware() {
-        this.server.use(express.json())
-        this.server.use(cors())
-    }
+  private middleware() {
+    this.server.use(express.json());
+    this.server.use(cors());
+  }
 
-    private routes() {
-        this.server.use(routes)
-        this.server.use(EpisodesRoute)
-        this.server.use(Charsroutes)
-        this.server.use(RelativesRoutes)
-        this.server.use(BooksRoutes)
-        this.server.use(WeirdmageddonRoutes)
-        this.server.use(CreaturesRoutes)
-    }
+  private routes() {
+    this.server.use(routes);
+    this.server.use(EpisodesRoute);
+    this.server.use(Charsroutes);
+    this.server.use(RelativesRoutes);
+    this.server.use(BooksRoutes);
+    this.server.use(WeirdmageddonRoutes);
+    this.server.use(CreaturesRoutes);
+  }
 }
 
-export default new App().server;
+const app = new App().server;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+export default app;
