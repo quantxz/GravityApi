8d76c11987cc7c3c0b035a3f99234a6a352896f0
quantxz <indiedev.core@gmail.com>
2023-07-02 19:12:11 -0300

    doc update

--- src/routes/main.routes.ts
+++ src/routes/main.routes.ts
@@ -1,20 +1,11 @@
 import { Request, Response, Router } from "express";
+import SwaggerUi from "swagger-ui-express";
 import { sql } from "@vercel/postgres";
+import swaggerDocs from '../configs/swagger.json'
 
 const routes: Router = Router();
 
-routes.get("/", async (req: Request, res: Response) => {
-    try {
-        const query = 'SELECT * FROM episodes';
-        const { rows } = await sql.query(query);
-        
-        return res.status(500).json(rows)
-
-    } catch (err) {
-        console.error('Error while executing query:', err);
-        return res.status(500).json(err);
-    }
-});
+routes.get("/", SwaggerUi.serve, SwaggerUi.setup(swaggerDocs));
 
 export default routes;
 
