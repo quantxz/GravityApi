4d189e6905e0964f086880648ab4593f1c396610
quantxz <indiedev.core@gmail.com>
2023-07-02 18:20:08 -0300

    update

--- src/app/controllers/relatives/MinorCharsRelatives.Controller.ts
+++ src/app/controllers/relatives/MinorCharsRelatives.Controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { sql } from "@vercel/postgres";
+import { QueryResult, sql } from "@vercel/postgres";
 import { CreateRelatives } from "../../models/relatives/MainCharsRealatives";
 import { CreateMinorsRelatives } from "../../models/relatives/MinorCharsRelatives";
 
@@ -26,6 +26,44 @@ class MinorsRelativesController {
             
             const { rows } = await sql.query(query);
             return res.status(200).json(rows);
+
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    }
+    
+    public async ViewSpecifyRelativeById(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+
+            const query = "SELECT * FROM minoscharsrelatives WHERE id = $1";
+            const values = [id];
+            
+            sql.query(query, values, (err: Error, result: QueryResult<any>) => {
+              if (err) {
+                return res.status(500).json(err);
+              }
+              return res.status(200).json(result.rows);
+            });
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    }
+
+    public async ViewSpecifyRelativeByName(req: Request, res: Response) {
+        try {
+            const { name } = req.params;
+
+            const query = "SELECT * FROM minoscharsrelatives WHERE charactername LIKE $1"
+      
+            const values = [name]
+      
+            sql.query(query, values, (err: Error, result: QueryResult<any>) => {
+              if (err) {
+                return res.status(500).json(err);
+              }
+              return res.status(200).json(result.rows);
+            });
 
         } catch(err) {
             return res.status(500).json(err);
