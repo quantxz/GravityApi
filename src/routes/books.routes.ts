0f0e1168121b925581eab815214f925a57cabb37
quantxz <indiedev.core@gmail.com>
2023-07-02 17:57:27 -0300

    update

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -4,9 +4,9 @@ const BooksRoutes: Router = Router();
 
 BooksRoutes.get("/books", BooksControllers.ViewBooks); //feito
 
-BooksRoutes.get("/books/:id", BooksControllers.ViewSpecifyBookById); //feito
+BooksRoutes.get("/books/id/:id", BooksControllers.ViewSpecifyBookById); //feito
 
-BooksRoutes.get("/books/:name", BooksControllers.ViewSpecifyBookByName); //feito
+BooksRoutes.get("/books/name/:name", BooksControllers.ViewSpecifyBookByName); //feito
 
 BooksRoutes.post("/books", BooksControllers.InsertBook); //feito
 
