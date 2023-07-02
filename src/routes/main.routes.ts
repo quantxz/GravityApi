5c344c60dafc107353fb05b38c1b6e8577dd7df6
quantxz <indiedev.core@gmail.com>
2023-07-02 19:28:59 -0300

    doc update

--- src/routes/main.routes.ts
+++ src/routes/main.routes.ts
@@ -5,8 +5,12 @@ import swaggerDocs from '../configs/swagger.json'
 
 const routes: Router = Router();
 
+var options = {
+    explorer: true
+};
+
 routes.use('/', SwaggerUi.serve);
-routes.get('/', SwaggerUi.setup(swaggerDocs));
+routes.get('/', SwaggerUi.setup(swaggerDocs, options));
 
 export default routes;
 
