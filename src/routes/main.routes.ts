77ba71deb1250ef4b969a92bbcadf2324b3ad9c0
quantxz <indiedev.core@gmail.com>
2023-07-02 19:49:37 -0300

    doc update

--- src/routes/main.routes.ts
+++ src/routes/main.routes.ts
@@ -1,20 +1,11 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import SwaggerUi from "swagger-ui-express";
+import swaggerDocs from '../configs/swagger.json';
 import "swagger-ui-dist/swagger-ui.css";
-import { sql } from "@vercel/postgres";
-import swaggerDocs from '../configs/swagger.json'
 
 const routes: Router = Router();
 
-const swaggerOptions = {
-    swaggerOptions: {
-      // Defina as opções do Swagger UI, incluindo o tema de estilo pré-definido
-      theme: 'feeling-blue', // Substitua "some-predefined-theme" pelo nome do tema desejado
-    },
-  };
-
 routes.use('/', SwaggerUi.serve);
-routes.get('/', SwaggerUi.setup(swaggerDocs, swaggerOptions));
+routes.get('/', SwaggerUi.setup(swaggerDocs));
 
 export default routes;
-
