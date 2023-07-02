67536c8e4c4e4f43a748d302bf6ad56a32ce4f79
quantxz <indiedev.core@gmail.com>
2023-07-02 20:10:50 -0300

    doc update

--- src/routes/main.routes.ts
+++ src/routes/main.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import SwaggerUi from "swagger-ui-express";
 import { SwaggerOptions } from "swagger-ui-express";
 import swaggerDocs from '../configs/swagger.json';
-import "swagger-ui-dist/swagger-ui.css";
+import "swagger-ui-dist";
 ; // Substitua pelo caminho correto para o arquivo CSS
 
 const routes: Router = Router();
@@ -11,7 +11,7 @@ const SwaggerOptions = {
     swaggerOptions: {
       theme: 'default', 
     },
-  };
+};
 
 routes.use('/', SwaggerUi.serve);
 routes.get('/', SwaggerUi.setup(swaggerDocs, SwaggerOptions));
