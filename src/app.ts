cb534c734c8994f2aa540a92943e0845aa74200e
quantxz <indiedev.core@gmail.com>
2023-07-03 19:44:54 -0300

    doc update

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,7 @@ import BooksRoutes from './routes/books.routes';
 import WeirdmageddonRoutes from './routes/Weirdmageddon.routes';
 import CreaturesRoutes from './routes/creatures.routes';
 import routes from './routes/main.routes';
-import path from 'path';
-import bodyParser from 'body-parser';
-import { SwaggerUIBundle } from 'swagger-ui-dist';
-
-const options = {
-  CustomCss: process.env.STYLE,
-  customSiteTitle: "The Words That I Know API - Swagger"
-};
-
-const ROOT_FOLDER = path.join(__dirname, '..');
-const SRC_FOLDER = path.join(ROOT_FOLDER, 'src');
+import '../node_modules/swagger-ui-dist/swagger-ui.css'
 
 class App {
   public server: express.Application;
@@ -33,7 +23,7 @@ class App {
   private middleware() {
     this.server.use(cors());
     this.server.use(express.json());
-    this.server.use('/api-docs', swaggerUI.serve, express.static(path.join(SRC_FOLDER, 'public')), swaggerUI.setup(swaggerDocs, options));
+    this.server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
   }
 
   private routes() {
@@ -55,13 +45,5 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
-app.use(express.static(__dirname));
-app.use('*.css', (req, res, next) => {
-    res.set('Content-Type', 'text/css');
-    next();
-});
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 export default app;
