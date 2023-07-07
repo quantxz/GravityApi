9785013c9c0e80b32d90031b71fc59cd4cfa2654
quantxz <indiedev.core@gmail.com>
2023-07-07 15:39:19 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Response } from 'express';
+const  helmet = require("helmet")
 import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import swaggerDocs from './swagger/jsons/swagger.json';
@@ -11,10 +12,6 @@ import CreaturesRoutes from './routes/creatures.routes';
 import routes from './routes/main.routes';
 import path from 'path';
 
-const options = {
-  customCssUrl: 'https://gf-db-transfer-quantxz.vercel.app/style.css',
-  customSiteTitle: "The Words That I Know API - Swagger"
-};
 
 class App {
   public server: express.Application;
@@ -30,7 +27,13 @@ class App {
     this.server.use(express.json());
     this.server.use(express.static('src'));
     this.server.use(express.static('Public'));
-    this.server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs, options));
+    this.server.use(helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'", 'data:', 'https://www.google-analytics.com'],
+        imgSrc: ["'self'", 'https://i.ibb.co'],
+      },
+    }));
+    this.server.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
   }
 
   private routes() {
